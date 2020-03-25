cd551f5376f30d31e3c89307b147d74dc892e8b5
Behnam <behnamsh72@gmail.com>
2020-03-26 03:56:05 +0430

    Added Specialities Map Impl.

---
+++ pet-clinic-data/src/main/java/behnam/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package behnam.springframework.sfgpetclinic.services;
+
+import behnam.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
