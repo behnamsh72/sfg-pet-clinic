788ab0d69b01663a3f33f9123d5c4eaeae92e981
Behnam <behnamsh72@gmail.com>
2020-04-01 02:17:19 +0430

    update vet to use JPAg

--- pet-clinic-data/src/main/java/behnam/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/behnam/springframework/sfgpetclinic/model/Vet.java
@@ -1,10 +1,16 @@
 package behnam.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
-    private Set<Speciality> specialities=new HashSet<>();
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_spacialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
+    private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
         return specialities;
